867559632695e2b42fd014e4c8896ca3534a5956
Noveign <harkedrx@gmail.com>
2025-05-30 16:10:39 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -34,61 +34,49 @@ local function vapeGithubRequest(scripturl)
 		task.delay(15, function()
 			if not res and not errorPopupShown then 
 				errorPopupShown = true
-				displayErrorPopup("The connection to github is taking a while, Please be patient.")
+				displayErrorPopup("The connection to GitHub is taking a while, please be patient.")
 			end
 		end)
-		suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+		suc, res = pcall(function() 
+			return game:HttpGet("https://raw.githubusercontent.com/Noveign/Vape2023/main/"..scripturl, true) 
+		end)
 		if not suc or res == "404: Not Found" then
-			displayErrorPopup("Failed to connect to github : vape/"..scripturl.." : "..res)
+			displayErrorPopup("Failed to connect to GitHub : vape/"..scripturl.." : "..res)
 			error(res)
 		end
-		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
+		if scripturl:find(".lua") then 
+			res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res 
+		end
 		writefile("vape/"..scripturl, res)
 	end
 	return readfile("vape/"..scripturl)
 end
 
+-- Skip commit hash check, always pull from main
 if not shared.VapeDeveloper then 
-	local commit = "main"
-	for i,v in pairs(game:HttpGet("https://github.com/7GrandDadPGN/VapeV4ForRoblox"):split("\n")) do 
-		if v:find("commit") and v:find("fragment") then 
-			local str = v:split("/")[5]
-			commit = str:sub(0, str:find('"') - 1)
-			break
+	if isfolder("vape") then 
+		for _,v in pairs({"vape/Universal.lua", "vape/NewMainScript.lua", "vape/GuiLibrary.lua"}) do 
+			if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+				delfile(v)
+			end 
 		end
-	end
-	if commit then
-		if isfolder("vape") then 
-			if ((not isfile("vape/commithash.txt")) or (readfile("vape/commithash.txt") ~= commit or commit == "main")) then
-				for i,v in pairs({"vape/Universal.lua", "vape/MainScript.lua", "vape/GuiLibrary.lua"}) do 
-					if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
-						delfile(v)
-					end 
-				end
-				if isfolder("vape/CustomModules") then 
-					for i,v in pairs(listfiles("vape/CustomModules")) do 
-						if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
-							delfile(v)
-						end 
-					end
-				end
-				if isfolder("vape/Libraries") then 
-					for i,v in pairs(listfiles("vape/Libraries")) do 
-						if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
-							delfile(v)
-						end 
-					end
-				end
-				writefile("vape/commithash.txt", commit)
+		if isfolder("vape/CustomModules") then 
+			for _,v in pairs(listfiles("vape/CustomModules")) do 
+				if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+					delfile(v)
+				end 
 			end
-		else
-			makefolder("vape")
-			writefile("vape/commithash.txt", commit)
+		end
+		if isfolder("vape/Libraries") then 
+			for _,v in pairs(listfiles("vape/Libraries")) do 
+				if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+					delfile(v)
+				end 
+			end
 		end
 	else
-		displayErrorPopup("Failed to connect to github, please try using a VPN.")
-		error("Failed to connect to github, please try using a VPN.")
+		makefolder("vape")
 	end
 end
 
-return loadstring(vapeGithubRequest("MainScript.lua"))()
+return loadstring(vapeGithubRequest("NewMainScript.lua"))()
